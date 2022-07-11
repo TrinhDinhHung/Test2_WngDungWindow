26c6efbb1799af4054c3ee1176329c50eeb41d85
Your Name <dev.dinhhung.011120@gmail.com>
2022-07-11 15:42:32 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace test2_winfrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbButton = new System.Windows.Forms.GroupBox();
+            this.btnDB = new System.Windows.Forms.Button();
+            this.btnACC = new System.Windows.Forms.Button();
+            this.btnCompany = new System.Windows.Forms.Button();
+            this.btnBookie = new System.Windows.Forms.Button();
+            this.btnBot = new System.Windows.Forms.Button();
+            this.btnChanel = new System.Windows.Forms.Button();
+            this.btnRule = new System.Windows.Forms.Button();
+            this.btnADD = new System.Windows.Forms.Button();
+            this.lvwDB = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvwACC = new System.Windows.Forms.ListView();
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader13 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader14 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader15 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.grbButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbButton
+            // 
+            this.grbButton.BackColor = System.Drawing.Color.DarkKhaki;
+            this.grbButton.Controls.Add(this.btnRule);
+            this.grbButton.Controls.Add(this.btnChanel);
+            this.grbButton.Controls.Add(this.btnBot);
+            this.grbButton.Controls.Add(this.btnBookie);
+            this.grbButton.Controls.Add(this.btnCompany);
+            this.grbButton.Controls.Add(this.btnACC);
+            this.grbButton.Controls.Add(this.btnDB);
+            this.grbButton.Location = new System.Drawing.Point(-4, 0);
+            this.grbButton.Name = "grbButton";
+            this.grbButton.Size = new System.Drawing.Size(554, 69);
+            this.grbButton.TabIndex = 0;
+            this.grbButton.TabStop = false;
+            // 
+            // btnDB
+            // 
+            this.btnDB.BackColor = System.Drawing.Color.Yellow;
+            this.btnDB.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDB.Location = new System.Drawing.Point(6, 8);
+            this.btnDB.Name = "btnDB";
+            this.btnDB.Size = new System.Drawing.Size(69, 44);
+            this.btnDB.TabIndex = 0;
+            this.btnDB.Text = "Dashboard";
+            this.btnDB.UseVisualStyleBackColor = false;
+            this.btnDB.Click += new System.EventHandler(this.btnDB_Click);
+            // 
+            // btnACC
+            // 
+            this.btnACC.BackColor = System.Drawing.Color.Yellow;
+            this.btnACC.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnACC.Location = new System.Drawing.Point(85, 8);
+            this.btnACC.Name = "btnACC";
+            this.btnACC.Size = new System.Drawing.Size(69, 44);
+            this.btnACC.TabIndex = 0;
+            this.btnACC.Text = "Account";
+            this.btnACC.UseVisualStyleBackColor = false;
+            this.btnACC.Click += new System.EventHandler(this.btnACC_Click);
+            // 
+            // btnCompany
+            // 
+            this.btnCompany.BackColor = System.Drawing.Color.Yellow;
+            this.btnCompany.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCompany.Location = new System.Drawing.Point(164, 8);
+            this.btnCompany.Name = "btnCompany";
+            this.btnCompany.Size = new System.Drawing.Size(69, 44);
+            this.btnCompany.TabIndex = 0;
+            this.btnCompany.Text = "Company";
+            this.btnCompany.UseVisualStyleBackColor = false;
+            // 
+            // btnBookie
+            // 
+            this.btnBookie.BackColor = System.Drawing.Color.Yellow;
+            this.btnBookie.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBookie.Location = new System.Drawing.Point(243, 8);
+            this.btnBookie.Name = "btnBookie";
+            this.btnBookie.Size = new System.Drawing.Size(69, 44);
+            this.btnBookie.TabIndex = 0;
+            this.btnBookie.Text = "Bookie";
+            this.btnBookie.UseVisualStyleBackColor = false;
+            // 
+            // btnBot
+            // 
+            this.btnBot.BackColor = System.Drawing.Color.Yellow;
+            this.btnBot.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBot.Location = new System.Drawing.Point(322, 8);
+            this.btnBot.Name = "btnBot";
+            this.btnBot.Size = new System.Drawing.Size(69, 44);
+            this.btnBot.TabIndex = 0;
+            this.btnBot.Text = "Bot";
+            this.btnBot.UseVisualStyleBackColor = false;
+            // 
+            // btnChanel
+            // 
+            this.btnChanel.BackColor = System.Drawing.Color.Yellow;
+            this.btnChanel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnChanel.Location = new System.Drawing.Point(401, 8);
+            this.btnChanel.Name = "btnChanel";
+            this.btnChanel.Size = new System.Drawing.Size(69, 44);
+            this.btnChanel.TabIndex = 0;
+            this.btnChanel.Text = "Channel";
+            this.btnChanel.UseVisualStyleBackColor = false;
+            // 
+            // btnRule
+            // 
+            this.btnRule.BackColor = System.Drawing.Color.Yellow;
+            this.btnRule.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRule.Location = new System.Drawing.Point(480, 8);
+            this.btnRule.Name = "btnRule";
+            this.btnRule.Size = new System.Drawing.Size(69, 44);
+            this.btnRule.TabIndex = 0;
+            this.btnRule.Text = "Rule";
+            this.btnRule.UseVisualStyleBackColor = false;
+            // 
+            // btnADD
+            // 
+            this.btnADD.BackColor = System.Drawing.Color.Yellow;
+            this.btnADD.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnADD.Location = new System.Drawing.Point(569, 17);
+            this.btnADD.Name = "btnADD";
+            this.btnADD.Size = new System.Drawing.Size(75, 27);
+            this.btnADD.TabIndex = 1;
+            this.btnADD.Text = "add";
+            this.btnADD.UseVisualStyleBackColor = false;
+            // 
+            // lvwDB
+            // 
+            this.lvwDB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lvwDB.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvwDB.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvwDB.FullRowSelect = true;
+            this.lvwDB.GridLines = true;
+            this.lvwDB.HideSelection = false;
+            this.lvwDB.Location = new System.Drawing.Point(11, 75);
+            this.lvwDB.Name = "lvwDB";
+            this.lvwDB.Size = new System.Drawing.Size(626, 330);
+            this.lvwDB.TabIndex = 2;
+            this.lvwDB.UseCompatibleStateImageBehavior = false;
+            this.lvwDB.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "STT";
+            this.columnHeader1.Width = 75;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Account";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 99;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Bookie";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 96;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Channel";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 94;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Rule";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 103;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "S";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 69;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Status";
+            this.columnHeader7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader7.Width = 83;
+            // 
+            // lvwACC
+            // 
+            this.lvwACC.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lvwACC.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10,
+            this.columnHeader11,
+            this.columnHeader12,
+            this.columnHeader13,
+            this.columnHeader14,
+            this.columnHeader15});
+            this.lvwACC.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvwACC.ForeColor = System.Drawing.Color.Black;
+            this.lvwACC.FullRowSelect = true;
+            this.lvwACC.GridLines = true;
+            this.lvwACC.HideSelection = false;
+            this.lvwACC.Location = new System.Drawing.Point(12, 75);
+            this.lvwACC.Name = "lvwACC";
+            this.lvwACC.Size = new System.Drawing.Size(652, 330);
+            this.lvwACC.TabIndex = 2;
+            this.lvwACC.UseCompatibleStateImageBehavior = false;
+            this.lvwACC.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "STT";
+            this.columnHeader8.Width = 42;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Account";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader9.Width = 128;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Company";
+            this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader10.Width = 115;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "Bookie";
+            this.columnHeader11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader11.Width = 81;
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "Bot";
+            this.columnHeader12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader12.Width = 75;
+            // 
+            // columnHeader13
+            // 
+            this.columnHeader13.Text = "Channel";
+            this.columnHeader13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader13.Width = 79;
+            // 
+            // columnHeader14
+            // 
+            this.columnHeader14.Text = "Rule";
+            this.columnHeader14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader14.Width = 66;
+            // 
+            // columnHeader15
+            // 
+            this.columnHeader15.Text = "State";
+            this.columnHeader15.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 415);
+            this.Controls.Add(this.lvwACC);
+            this.Controls.Add(this.lvwDB);
+            this.Controls.Add(this.btnADD);
+            this.Controls.Add(this.grbButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbButton.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbButton;
+        private System.Windows.Forms.Button btnRule;
+        private System.Windows.Forms.Button btnChanel;
+        private System.Windows.Forms.Button btnBot;
+        private System.Windows.Forms.Button btnBookie;
+        private System.Windows.Forms.Button btnCompany;
+        private System.Windows.Forms.Button btnACC;
+        private System.Windows.Forms.Button btnDB;
+        private System.Windows.Forms.Button btnADD;
+        private System.Windows.Forms.ListView lvwDB;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ListView lvwACC;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+        private System.Windows.Forms.ColumnHeader columnHeader13;
+        private System.Windows.Forms.ColumnHeader columnHeader14;
+        private System.Windows.Forms.ColumnHeader columnHeader15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test2_winfrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDB_Click(object sender, EventArgs e)
+        {
+            lvwDB.Visible = true;
+            lvwACC.Visible = false;
+        }
+
+        private void btnACC_Click(object sender, EventArgs e)
+        {
+            lvwACC.Visible = true;
+            lvwDB.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDB.PerformClick();
+            Khoi_tao_du_lieu();
+        }
+
+        private void Khoi_tao_du_lieu()
+        {
+            ListViewItem lvDB = new ListViewItem("1");
+            lvDB.SubItems.Add("Cam");
+            lvDB.SubItems.Add("Xoài");
+            lvDB.SubItems.Add("Ổi");
+            lvDB.SubItems.Add("1000/1000");
+            lvDB.SubItems.Add("Táo");
+            lvDB.SubItems.Add("Run/Stop");
+            lvwDB.Items.Add(lvDB);
+            ListViewItem lvDB2 = new ListViewItem("2");
+            lvDB2.SubItems.Add("Mít");
+            lvDB2.SubItems.Add("Mận");
+            lvDB2.SubItems.Add("Chanh");
+            lvDB2.SubItems.Add("1000/1000");
+            lvDB2.SubItems.Add("Hồng");
+            lvDB2.SubItems.Add("Run/Stop");
+            lvwDB.Items.Add(lvDB2);
+            ListViewItem lvDB3 = new ListViewItem("3");
+            lvDB3.SubItems.Add("Lê");
+            lvDB3.SubItems.Add("Bòn Bon");
+            lvDB3.SubItems.Add("Cóc");
+            lvDB3.SubItems.Add("1000/1000");
+            lvDB3.SubItems.Add("Táo");
+            lvDB3.SubItems.Add("Run/Stop");
+            lvwDB.Items.Add(lvDB3);
+            ListViewItem lvAC = new ListViewItem("1");
+            lvAC.SubItems.Add("Nội thất");
+            lvAC.SubItems.Add("Thú Cưng");
+            lvAC.SubItems.Add("Xây dựng");
+            lvAC.SubItems.Add("Pet");
+            lvAC.SubItems.Add("Táo");
+            lvAC.SubItems.Add("1000/1000");
+            lvAC.SubItems.Add("Run/Stop");
+            lvwACC.Items.Add(lvAC);
+            ListViewItem lvAC2 = new ListViewItem("2");
+            lvAC2.SubItems.Add("Ngoại thất");
+            lvAC2.SubItems.Add("Thú Cưng");
+            lvAC2.SubItems.Add("Thiết kế");
+            lvAC2.SubItems.Add("Pet");
+            lvAC2.SubItems.Add("Đa dụng");
+            lvAC2.SubItems.Add("1000/1000");
+            lvAC2.SubItems.Add("Run/Stop");
+            lvwACC.Items.Add(lvAC2);
+            ListViewItem lvAC3 = new ListViewItem("3");
+            lvAC3.SubItems.Add("Kinh tế");
+            lvAC3.SubItems.Add("Maketing");
+            lvAC3.SubItems.Add("sale");
+            lvAC3.SubItems.Add("User");
+            lvAC3.SubItems.Add("Sad");
+            lvAC3.SubItems.Add("1000/1000");
+            lvAC3.SubItems.Add("Run/Stop");
+            lvwACC.Items.Add(lvAC3);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult thoat = MessageBox.Show("Thoát Không?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (DialogResult.No == thoat)
+                e.Cancel = true;
+        }
+    }
+}
